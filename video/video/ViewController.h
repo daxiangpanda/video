c679dc48fb9c6c3b8a1f2f1b9ed5bfacbd5a49a3
刘鑫忠 <xinzhongliu@sohu-inc.com>
2017-09-18 20:59:54 +0800

    first commit

---
+++ video/video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  video
+//
+//  Created by 刘鑫忠 on 2017/9/18.
+//  Copyright © 2017年 刘鑫忠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
