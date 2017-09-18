e668a5ab8d6a2ac6505f2383657a5715f73f06b0
刘鑫忠 <xinzhongliu@sohu-inc.com>
2017-09-18 21:37:49 +0800

    0937

--- video/video/ViewController.m
+++ video/video/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <TZImagePickerController.h>
 
-@interface ViewController ()
+@interface ViewController ()<TZImagePickerControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIButton *openImagePicker;
 
 @end
 
@@ -16,9 +19,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.openImagePicker addTarget:self action:@selector(openImagePickerButtonTapped) forControlEvents:UIControlEventTouchUpInside];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+- (void)openImagePickerButtonTapped{
+    TZImagePickerController *imagePickerVc = [[TZImagePickerController alloc] initWithMaxImagesCount:9 delegate:self];
+    
+    // You can get the photos by block, the same as by delegate.
+    // 你可以通过block或者代理，来得到用户选择的照片.
+//    [imagePickerVc setDidFinishPickingPhotosHandle:^(NSArray<UIImage *> *photos, NSArray *assets) {
+//        
+//    }];
+    [self presentViewController:imagePickerVc animated:YES completion:nil];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
